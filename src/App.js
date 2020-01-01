3b41c59709952e059709d31d5fe3018474aefa70
three4c <three4c@gmail.com>
2020-01-01 11:58:26 +0900

    セットアップ

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import client from "./client";
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log(GITHUB_TOKEN);
+const ME = gql`
+  query me {
+    user(login: "three4c") {
+      name
+      avatarUrl
+    }
+  }
+`;
 
 const App = () => {
-  return <div>ほげ</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>ほげ</div>
+      <Query query={ME}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+
+          return <div>{data.user.name}</div>;
+        }}
+      </Query>
+    </ApolloProvider>
+  );
 };
 
 export default App;
